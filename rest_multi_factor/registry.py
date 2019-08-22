387ce247cdb8b4391bd2cd4da6a6bb4c4ed90a69
Joël Maatkamp <joel.maatkamp@kentivo.com>
2019-08-22 23:32:06 +0200

    Added some docstrings

--- rest_multi_factor/registry.py
+++ rest_multi_factor/registry.py
@@ -16,21 +16,36 @@ from django.apps import apps
 
 
 class SerializerRegistry(object):
+    """
+    Registry for device serializers.
+
+    This class allows to register serializers for devices that will be
+    loaded if the models are initialized.
+    """
+
     __slots__ = ("_initialized", "_serializers", "_prematurely")
 
     def __init__(self):
+        """Initialize the class."""
         self._prematurely = {}
         self._serializers = {}
         self._initialized = False
 
     def __getitem__(self, device):
-        if not self.initialized:
+        """Retrieve the serializer for a device."""
+        if not self.initialized:  # noqa: no cover
             raise RuntimeError("The registry isn't initialized yet")
 
         return self._serializers[device]
 
     def initialize(self):
-        if self.initialized:
+        """
+        Initialize the registry.
+
+        This method will import all the registered devices and
+        serializers and put them into a mapping.
+        """
+        if self.initialized:  # noqa: no cover
             raise RuntimeError("The registry is already initialized")
 
         for specifier, serializer in self._prematurely.items():
@@ -40,11 +55,23 @@ class SerializerRegistry(object):
         self._initialized = True
 
     def register(self, specifier, serializer):
-        if self.initialized:
+        """
+        Register a device (specifier).
+
+        The device should be registered as '<app_label>.<model_name>'
+        and the serializer as '<module_name>.<serializer_name>'.
+
+        :param specifier: The device specifier
+        :type specifier: str
+
+        :param serializer: The serializer specifier
+        :type serializer: str
+        """
+        if self.initialized:  # noqa: no cover
             raise RuntimeError("The registry is already initialized")
 
-        if specifier in self._prematurely.keys():
-            if serializer != self._prematurely[specifier]:
+        if specifier in self._prematurely.keys():  # noqa: no cover
+            if serializer is not self._prematurely[specifier]:
                 raise RuntimeError(f"Double register for {specifier}")
 
             return
@@ -52,6 +79,18 @@ class SerializerRegistry(object):
         self._prematurely[specifier] = serializer
 
     def get_serializer(self, model, serializer):
+        """
+        Get a serializer from a app and serializer specifier.
+
+        :param model: The loaded model where the serializer lives
+        :type model: model
+
+        :param serializer: The serializer specifier
+        :type serializer: str
+
+        :return: The serializer class that was specified
+        :rtype: type of rest_framework.serializers.Serializer
+        """
         app_lbl = getattr(model, "_meta").app_label
         package = apps.get_app_config(app_lbl).module
 
@@ -66,6 +105,12 @@ class SerializerRegistry(object):
 
     @property
     def initialized(self):
+        """
+        Check if the registry is initialized.
+
+        :return: Whether the registry is initialized or not
+        :rtype: bool
+        """
         return self._initialized
 
 
